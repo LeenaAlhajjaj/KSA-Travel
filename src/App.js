e4332cfa663096eefc52b54daf178f08fa63a83b
LeenaAlhajjaj <75483209+LeenaAlhajjaj@users.noreply.github.com>
2021-04-17 22:10:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Components/pages/Home';
+import Places from './Components/pages/Places';
+import About from './Components/pages/About';
 
 
 function App() {
   return (
     <>
     <Router>
-    <Navbar />
-    <Switch>
-<Route path='/' exact Components= {Home} />
-
-    </Switch>
-    </Router>
+      <Navbar />
+        <Switch>
+           <Route path='/' exact component= {Home} />
+           <Route path='/places' exact component= {Places} />
+           <Route path='/about' exact component= {About} />
+        </Switch>
+      </Router>
     </>
   );
 }
