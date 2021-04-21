95c71916f4bd7c3b6c379e8b551e8c95ddb1f9c2
LeenaAlhajjaj <75483209+LeenaAlhajjaj@users.noreply.github.com>
2021-04-21 20:25:13 +0300

    Update CardsItem.js

--- src/Components/CardsItem.js
+++ src/Components/CardsItem.js
@@ -6,7 +6,7 @@ function CardsItem(props) {
     <>
       <li className='cards-item'>
         <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+          <figure className='cards__item__pic-wrap' data-category={props.lable}>
             <img
               className='cards__item__img'
               alt='Travel Image'
